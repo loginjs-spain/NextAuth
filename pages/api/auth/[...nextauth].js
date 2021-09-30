b190583aa7cb76dd8d1803a0afb80ddaac2b881c
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-01 00:33:57 +0200

    sc

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,23 +6,31 @@ const options = {
         Providers.GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET
-        })
-        // Providers.Twitter({
-        //     clientId: "",
-        //     clientSecret: ""
-        // }),
-        // Providers.Email({
-        //     server: {
-        //         host: "",
-        //         port: "",
-        //         auth: {
-        //             user: "",
-        //             pass: ""
-        //         }
-        //     },
-        //     form: "",
-        // })
-    ]
+        }),
+        Providers.Google({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        Providers.Email({
+            server: {
+              host: process.env.EMAIL_SERVER_HOST,
+              port: process.env.EMAIL_SERVER_PORT,
+              auth: {
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD,
+              },
+            },
+            from: process.env.EMAIL_FROM,
+          }),
+    ],
+    pages: {
+        signIn : "/signin",
+    },
+    database: {
+        type: "sqlite",
+        database: ":memory:",
+        synchronize: true,
+      },
 };
 
 export default (req, res) => NextAuth(req, res, options);
